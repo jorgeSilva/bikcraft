290fa5a79ba4720fb11c02a6ae2d48980ad01eb8
Jorge Silva <jorgelfsilva5@gmail.com>
2023-02-08 16:03:58 -0300

    Adicionando parametros nas urls para transferir informação para compra

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
+//ativar os links do menu
 const links = document.querySelectorAll(".header__menu a")
 
-function ativar_link(item){
+links.forEach(function ativar_link(item){
   const url = location.href
   const href = item.href
 
   if(url.includes(href)){
     item.classList.add("ativo")
   }
-}
+})
 
-links.forEach(ativar_link)
+//ativar itens do orçamento
+const parametros = new URLSearchParams(location.search)
+
+parametros.forEach(function(item){
+  const elemento = document.getElementById(item)
+  if(elemento){
+    elemento.checked = true
+  }
+})
